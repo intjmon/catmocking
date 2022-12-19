b9dcbff3610fd119c7db0b6422779eafcdbc9869
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-19 22:53:09 +0900

    route를 분리하기 위해 cats 관련된 내용을 이동시킴

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import catsRouter from './cats/cats.routes';
 
 const app: express.Express = express();
 const port: number = 3000;
@@ -16,63 +16,7 @@ app.use(
 // json을 읽을 수 있도록 미들웨어 추가
 app.use(express.json());
 
-//* READ 모든 데이터
-app.get('/cats', (req, res) => {
-  try {
-    const cats = Cat;
-    //throw new Error('DB connect Error');
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
-
-//* READ 특정 데이터
-app.get('/cats/:id', (req, res) => {
-  try {
-    const params = req.params;
-    console.log('params:', params);
-    const cats = Cat.find((cat) => cat.id === params.id);
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
-
-//* CREATE new cat
-app.post('/cats', (req, res) => {
-  try {
-    const data = req.body;
-    console.log('data:', data);
-    Cat.push(data);
-    console.log('Cat:', Cat);
-    res.status(200).send({
-      success: true,
-      data: {},
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
+app.use(catsRouter);
 
 //* 404 middleware
 app.use(
