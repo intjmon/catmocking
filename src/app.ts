99d9dfe7a24289e2267f7308c011e4a7f45d9aed
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-19 22:28:31 +0900

    read all / read one caat

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { Cat, CatType } from './app.model';
 const app: express.Express = express();
 const port: number = 3000;
 
+//* logging middleware
 app.use(
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log(req.rawHeaders[1]);
@@ -12,30 +13,52 @@ app.use(
   }
 );
 
-app.get(
-  '/cats/som',
+//* READ 모든 데이터
+app.get('/cats', (req, res) => {
+  try {
+    const cats = Cat;
+    //throw new Error('DB connect Error');
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (e: any) {
+    res.status(400).send({
+      success: false,
+      error: e.message,
+    });
+  }
+});
+
+//* READ 특정 데이터
+app.get('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    console.log('params:', params);
+    const cats = Cat.find((cat) => cat.id === params.id);
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (e: any) {
+    res.status(400).send({
+      success: false,
+      error: e.message,
+    });
+  }
+});
+
+//* 404 middleware
+app.use(
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('this is som middleware');
-    next();
+    console.log('this is 404');
+    res.status(404).send('404 not found');
   }
 );
-
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
-});
-
-app.get('/cats/blue', (req: express.Request, res: express.Response) => {
-  res.send({ blue: Cat[0] });
-});
-
-app.get('/cats/som', (req: express.Request, res: express.Response) => {
-  res.send({ som: Cat[1] });
-});
-
-app.use((req, res, next) => {
-  console.log('this is 404');
-  res.status(404).send('404 not found');
-});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
