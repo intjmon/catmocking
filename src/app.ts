1601f11e503e87927a1ae24f5efd8b6525b38853
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-19 21:29:01 +0900

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,27 @@ import { Cat, CatType } from './app.model';
 const app: express.Express = express();
 const port: number = 3000;
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('this this longging middleware');
+  next();
+});
+
+app.get('/cats/som', (req, res, next) => {
+  console.log('this is som middleware');
+  next();
+});
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
+});
+
+app.get('/cats/blue', (req: express.Request, res: express.Response) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get('/cats/som', (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
 });
 
 app.listen(port, () => {
