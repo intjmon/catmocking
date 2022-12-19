ec4299fb944c7f479cf3add6b121c6362a519caf
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-19 19:10:52 +0900

    first

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import * as express from 'express';
 const app: express.Express = express();
 const port: number = 3000;
 
+const data = [1, 2, 3, 4];
+
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({ hello: 'world' });
+  res.send({ data });
 });
 
 app.listen(port, () => {
