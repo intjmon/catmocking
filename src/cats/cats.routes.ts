5ad33e2797a7cb956abb76baee53b066b07d3700
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-20 00:46:07 +0900

    라우터와 비즈니스 로직 분리 - 라우터

--- src/cats/cats.routes.ts
+++ src/cats/cats.routes.ts
@@ -1,143 +1,23 @@
-import { Cat, CatType } from './app.model';
 import { Router } from 'express';
+import { NextFunction } from 'express';
+
+import {
+  readAllCat,
+  readCat,
+  createCat,
+  updateCat,
+  updatePartialCat,
+  deleteCat,
+} from './cats.service';
 
 const router = Router();
 
-//* READ 모든 데이터
-router.get('/cats', (req, res) => {
-  try {
-    const cats = Cat;
-    //throw new Error('DB connect Error');
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
+console.log('router');
+router.get('/cats', readAllCat);
+router.get('/cats/:id', readCat);
+router.post('/cats', createCat);
+router.put('/cats/:id', updateCat);
+router.patch('/cats/:id', updatePartialCat);
+router.delete('/cats/:id', deleteCat);
 
-//* READ 특정 데이터
-router.get('/cats/:id', (req, res) => {
-  try {
-    const params = req.params;
-    console.log('params:', params);
-    const cats = Cat.find((cat) => cat.id === params.id);
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
-
-//* CREATE new cat
-router.post('/cats', (req, res) => {
-  try {
-    const data = req.body;
-    console.log('data:', data);
-    Cat.push(data);
-    console.log('Cat:', Cat);
-    res.status(200).send({
-      success: true,
-      data: {},
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
-
-//* PUT 특정 id의 내용 전체 업데이트
-router.put('/cats/:id', (req, res) => {
-  try {
-    const parmas = req.params;
-    const body = req.body;
-    let result;
-    console.log('body:', body);
-
-    Cat.forEach((cat) => {
-      if (cat.id === parmas.id) {
-        cat = body;
-        result = cat;
-      }
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
-
-//* PATCH 부분 입데이트
-router.patch('/cats/:id', (req, res) => {
-  try {
-    const parmas = req.params;
-    const body = req.body;
-    let result;
-    console.log('body:', body);
-
-    Cat.forEach((cat) => {
-      if (cat.id === parmas.id) {
-        cat = { ...cat, ...body };
-        result = cat;
-      }
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
-
-//* DELETE 삭제
-router.delete('/cats/:id', (req, res) => {
-  try {
-    const parmas = req.params;
-    const body = req.body;
-    let result;
-    console.log('body:', body);
-
-    const newCat = Cat.filter((cat) => cat.id !== parmas.id);
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: newCat,
-      },
-    });
-  } catch (e: any) {
-    res.status(400).send({
-      success: false,
-      error: e.message,
-    });
-  }
-});
 export default router;
